ccd6c399bd9b43254fc221387e6e31920c2462f1
Tan Kucukhas <tankucukhas@hotmail.com>
2025-09-25 23:03:41 -0400

    Update homepage with new tailwind-plus structure and Karavan-specific content

--- src/components/catalyst/link.tsx
+++ src/components/catalyst/link.tsx
@@ -1,26 +1,21 @@
-import clsx from 'clsx'
+/**
+ * TODO: Update this component to use your client-side framework's link
+ * component. We've provided examples of how to do this for Next.js, Remix, and
+ * Inertia.js in the Catalyst documentation:
+ *
+ * https://catalyst.tailwindui.com/docs#client-side-router-integration
+ */
+
+import * as Headless from '@headlessui/react'
 import React, { forwardRef } from 'react'
 
-type LinkProps = {
-  className?: string
-  children: React.ReactNode
-} & React.ComponentPropsWithoutRef<'a'>
-
-export const Link = forwardRef<HTMLAnchorElement, LinkProps>(
-  ({ className, children, ...props }, ref) => {
-    return (
-      <a
-        ref={ref}
-        className={clsx(
-          'text-blue-600 hover:text-blue-500 transition-colors',
-          className
-        )}
-        {...props}
-      >
-        {children}
-      </a>
-    )
-  }
-)
-
-Link.displayName = 'Link'
+export const Link = forwardRef(function Link(
+  props: { href: string } & React.ComponentPropsWithoutRef<'a'>,
+  ref: React.ForwardedRef<HTMLAnchorElement>
+) {
+  return (
+    <Headless.DataInteractive>
+      <a {...props} ref={ref} />
+    </Headless.DataInteractive>
+  )
+})
